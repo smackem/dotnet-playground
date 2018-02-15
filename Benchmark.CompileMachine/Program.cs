74104f9dc3c5a678b352c6fb07b42bf35820f451
smackem <sm4ckem@outlook.com>
2018-02-15 16:49:06 +0100

    added bitcount benchmarks

--- Benchmark.CompileMachine/Program.cs
+++ Benchmark.CompileMachine/Program.cs
@@ -55,11 +55,164 @@ namespace Benchmark.CompileMachine
         }
     }
 
+    [MemoryDiagnoser]
+    public class BitCountBenchmarks
+    {
+        static readonly int[] BitCountsPerNibble = new[]
+        {
+            0, //0
+            1, //1
+            1, //2
+            2, //3
+            1, //4
+            2, //5
+            2, //6
+            3, //7
+            1, //8
+            2, //9
+            2, //10
+            3, //11
+            2, //12
+            3, //13
+            3, //14
+            4, //15
+        };
+
+        static readonly int[] BitCountsPerByte;
+
+        static BitCountBenchmarks()
+        {
+            BitCountsPerByte = new int[256];
+            var b = new BitCountBenchmarks();
+
+            for (int i = 0; i < 256; i++)
+                BitCountsPerByte[i] = b.BitCount_NaiveBitwise(i);
+        }
+
+        int BitCount_NaiveBitwise(int n)
+        {
+            var count = 0;
+            var u = (uint)n;
+
+            while (u > 0)
+            {
+                count += (int)(u & 1);
+                u >>= 1;
+            }
+
+            return count;
+        }
+
+        int BitCount_Nibblewise(int n)
+        {
+            var count = 0;
+            var u = (uint)n;
+
+            while (u > 0)
+            {
+                count += BitCountsPerNibble[u & 0xf];
+                u >>= 4;
+            }
+
+            return count;
+        }
+
+        int BitCount_Bytewise(int n)
+        {
+            var count = 0;
+            var u = (uint)n;
+
+            while (u > 0)
+            {
+                count += BitCountsPerByte[u & 0xff];
+                u >>= 8;
+            }
+
+            return count;
+        }
+
+        int BitCount_BytewiseNoLoop(int n)
+        {
+            return BitCountsPerByte[(n & 0x000000ff) >> 0]
+                 + BitCountsPerByte[(n & 0x0000ff00) >> 8]
+                 + BitCountsPerByte[(n & 0x00ff0000) >> 16]
+                 + BitCountsPerByte[(n & 0xff000000) >> 24];
+        }
+
+        int BitCount_Swar(int i)
+        {
+            i = i - ((i >> 1) & 0x55555555);
+            i = (i & 0x33333333) + ((i >> 2) & 0x33333333);
+            return (((i + (i >> 4)) & 0x0F0F0F0F) * 0x01010101) >> 24;
+        }
+
+        [Benchmark]
+        public void NaiveBitwise()
+        {
+            if (BitCount_NaiveBitwise(0) != 0) throw new Exception();
+            if (BitCount_NaiveBitwise(1) != 1) throw new Exception();
+            if (BitCount_NaiveBitwise(0xf) != 4) throw new Exception();
+            if (BitCount_NaiveBitwise(0xff) != 8) throw new Exception();
+            if (BitCount_NaiveBitwise(0xffff) != 16) throw new Exception();
+            if (BitCount_NaiveBitwise(0xffffff) != 24) throw new Exception();
+            if (BitCount_NaiveBitwise(unchecked((int)0xfffffffe)) != 31) throw new Exception();
+        }
+
+        [Benchmark]
+        public void Nibblewise()
+        {
+            if (BitCount_Nibblewise(0) != 0) throw new Exception();
+            if (BitCount_Nibblewise(1) != 1) throw new Exception();
+            if (BitCount_Nibblewise(0xf) != 4) throw new Exception();
+            if (BitCount_Nibblewise(0xff) != 8) throw new Exception();
+            if (BitCount_Nibblewise(0xffff) != 16) throw new Exception();
+            if (BitCount_Nibblewise(0xffffff) != 24) throw new Exception();
+            if (BitCount_Nibblewise(unchecked((int)0xfffffffe)) != 31) throw new Exception();
+        }
+
+        [Benchmark]
+        public void Bytewise()
+        {
+            if (BitCount_Bytewise(0) != 0) throw new Exception();
+            if (BitCount_Bytewise(1) != 1) throw new Exception();
+            if (BitCount_Bytewise(0xf) != 4) throw new Exception();
+            if (BitCount_Bytewise(0xff) != 8) throw new Exception();
+            if (BitCount_Bytewise(0xffff) != 16) throw new Exception();
+            if (BitCount_Bytewise(0xffffff) != 24) throw new Exception();
+            if (BitCount_Bytewise(unchecked((int)0xfffffffe)) != 31) throw new Exception();
+        }
+
+        [Benchmark]
+        public void Bytewise_NoLoop()
+        {
+            if (BitCount_BytewiseNoLoop(0) != 0) throw new Exception();
+            if (BitCount_BytewiseNoLoop(1) != 1) throw new Exception();
+            if (BitCount_BytewiseNoLoop(0xf) != 4) throw new Exception();
+            if (BitCount_BytewiseNoLoop(0xff) != 8) throw new Exception();
+            if (BitCount_BytewiseNoLoop(0xffff) != 16) throw new Exception();
+            if (BitCount_BytewiseNoLoop(0xffffff) != 24) throw new Exception();
+            if (BitCount_BytewiseNoLoop(unchecked((int)0xfffffffe)) != 31) throw new Exception();
+        }
+
+        [Benchmark]
+        public void Swar()
+        {
+            if (BitCount_Swar(0) != 0) throw new Exception();
+            if (BitCount_Swar(1) != 1) throw new Exception();
+            if (BitCount_Swar(0xf) != 4) throw new Exception();
+            if (BitCount_Swar(0xff) != 8) throw new Exception();
+            if (BitCount_Swar(0xffff) != 16) throw new Exception();
+            if (BitCount_Swar(0xffffff) != 24) throw new Exception();
+            if (BitCount_Swar(unchecked((int)0xfffffffe)) != 31) throw new Exception();
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            BenchmarkRunner.Run<CompileMachineBenchmarks>();
+            //BenchmarkRunner.Run<CompileMachineBenchmarks>();
+            BenchmarkRunner.Run<BitCountBenchmarks>();
         }
     }
 }
